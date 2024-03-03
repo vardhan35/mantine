ee64149cdf1183e441035fd92da72e01a6a360e2
vardhan <harshwardhankalse03@gmail.com>
2024-03-04 02:08:50 +0530

    importing cards in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function HomePage() {
-  return <div>Home page</div>;
+import Users from "../src/components/Users";
+
+async function getUsers() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users");
+  return await data.json();
+}
+
+export default async function HomePage() {
+  const data = await getUsers();
+  return (
+    <div>
+      <Users users={data} />
+    </div>
+  );
 }
